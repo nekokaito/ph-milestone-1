3e21889748621bfd23b021dfffce4f601e6b695b
nekokaito <siddiqsazzad001@gmail.com>
2024-02-09 22:27:20 +0600

    dom getEMbyClass

--- Milestone-5/tour-of-DOM/dom.js
+++ Milestone-5/tour-of-DOM/dom.js
@@ -3,4 +3,9 @@ const Collection = document.getElementsByTagName('li');
 for (const i of Collection) {
     console.log(i);
 }
-console.log('Hello');
+let Title = document.getElementById('welcome-text');
+Title.innerText = 'Hola';
+console.log(Title);
+
+const Fav = document.getElementsByClassName('my-fav');
+console.log(Fav);
