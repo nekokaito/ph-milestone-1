4f6d3dce86532773ee9524d8cff4b09dc48101ec
nekokaito <siddiqsazzad001@gmail.com>
2024-02-11 22:51:14 +0600

    my biggest mistake js

--- Milestone-5/chat-DOM/chat.js
+++ Milestone-5/chat-DOM/chat.js
@@ -1,14 +1,41 @@
+
+let c = 0;
 document.getElementById('send-btn').addEventListener('click', function(){
-   
+    c++;
+    
     const chatBox = document.getElementById('chat-box');
     const chatValue = chatBox.value;
-    const chatContainer = document.getElementById('chat-container');
-    const newChat = document.createElement('div');
+    const chat = document.getElementById('chat-main');
+    const start = document.getElementById('chat-container-start');
+    const end = document.getElementById('chat-container-end');
     
-    newChat.classList.add('chat-bubble','chat-bubble-primary', 'mt-3');
-
-    newChat.innerText = chatValue;
-    chatContainer.appendChild(newChat);
-    chatBox.value = '';
+     if (c%2===1) {
+        let text = 'msg'+ c.toString();
+        const newChat = document.createElement('div');
+        const chatBubble = document.createElement('div');
+        newChat.classList.add('chat', 'chat-start', text);
+        chatBubble.classList.add('chat-bubble','chat-bubble-primary', 'mt-3');
+        chatBubble.innerText = chatValue;
+        chat.appendChild(newChat);
+        start.appendChild(chatBubble);
+        chatBox.value = '';
+     } 
+     else {
+        let text = 'msg'+c.toString();
+        const newChat = document.createElement('div');
+        const chatBubble = document.createElement('div');
+        newChat.classList.add('chat', 'chat-end', text);
+        chatBubble.classList.add('chat-bubble','chat-bubble-success', 'mt-3');
+        chatBubble.innerText = chatValue;
+        chat.appendChild(newChat);
+        end.appendChild(chatBubble);
+        chatBox.value = '';
+     }
+    
+    
+    
+    
+     console.log(c);
  
 })
+
