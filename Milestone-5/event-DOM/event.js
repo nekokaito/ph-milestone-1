969a6ea0e456219e25b930e03bdbc5a67f33bd3e
nekokaito <siddiqsazzad001@gmail.com>
2024-02-11 10:53:10 +0600

    some dom event

--- Milestone-5/event-DOM/event.js
+++ Milestone-5/event-DOM/event.js
@@ -2,3 +2,24 @@ function Color() {
     document.body.style.backgroundColor = '#1a1b26';
    return code;
 }
+
+document.getElementById('make-green').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'lime';
+})
+
+document.getElementById('btn-update').addEventListener('click', function(){
+    const inputOne = document.getElementById('input-one');
+    const valueOne = inputOne.value;
+    const inputTwo = document.getElementById('input-two');
+    const valueTwo = inputTwo.value;
+    const textOne  = document.getElementById('text-one');
+    textOne.innerText = valueOne;
+    const textTwo = document.getElementById('text-two');
+    textTwo.innerText = valueTwo;
+
+    inputOne.value =  '';
+    inputTwo.value = '';
+
+    
+
+})
