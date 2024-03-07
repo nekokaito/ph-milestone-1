9d2ffee7060948e38554b40b5a92d2bb0560ab2d
nekokaito <siddiqsazzad001@gmail.com>
2024-03-07 21:54:15 +0600

    added information

--- Milestone-7/React/hello-react/src/App.jsx
+++ Milestone-7/React/hello-react/src/App.jsx
@@ -8,6 +8,8 @@ function App() {
      <>
      <Hello></Hello>
       <Person></Person>
+      <h3>Salary Monthly</h3>
+      <Salary jan="5000" feb={6000} march={7510} april={10177}></Salary>
      </>
       
   )
@@ -15,7 +17,7 @@ function App() {
 
 function Hello() {
  return (
-         <h1>Hello World</h1>
+         <h1>Person Information</h1>
          
  ) 
 }
@@ -24,21 +26,54 @@ function Person() {
   const his_name = 'Kaito';
   const age = 24;
   const skills = ['Java', 'C/C++', 'JavaScript'];
+  const personStyle = {
+     border : '2px solid blue',
+     borderRadius : '16px',
+     margin: '10px',
+     padding: '10px',
+     fontWeight: 'bold'
+  }
 
   return (
-    <>
-     <p>Name : {his_name}</p>
+    <div style={personStyle}>
+      <p>Name : {his_name}</p>
      <p>Age : {age}</p>
+     <p>Skill</p>
      {
       skills.map((skill) => (
+        <>
+         <hr />
          <p>{skill}</p>
+        </>
+
       ) )
      }
-    </>
+    </div>
      
      
   )
 
+}
+function Salary({jan=0,feb=0,march=0,april=0}) {
+  const salaryStyle = {
+    border : '2px solid green',
+    borderRadius : '16px',
+    margin: '10px',
+    padding: '10px',
+    fontWeight: 'bold'
+ }
+
+  console.log(jan);
+  return (
+    <div style={salaryStyle}>
+    <p>January : {jan} ৳</p>
+    <p>Feb : {feb} ৳</p>
+    <p>March : {march} ৳</p>
+    <p>April : {april} ৳</p>
+    
+    </div>
+  )
+    
 }
 
 export default App
